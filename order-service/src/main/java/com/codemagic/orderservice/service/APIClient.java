fd8dc543ef8207a46eec406e8a53c624aaa842a1
Lalit Solanki <shobhitsingh.e28@gmail.com>
2024-04-08 13:04:16 +0530

    Added More Services & refactor older services accordingly

--- order-service/src/main/java/com/codemagic/orderservice/service/APIClient.java
+++ order-service/src/main/java/com/codemagic/orderservice/service/APIClient.java
@@ -5,7 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="http://localhost:8083",value = "INVENTORY-SERVICE")
+//@FeignClient(url="http://localhost:8083",value = "INVENTORY-SERVICE")
+@FeignClient(name="INVENTORY-SERVICE")
 public interface APIClient {
     @GetMapping("api/v1/inventory/{product-name}")
     public ResponseEntity<Boolean> isPresent(@PathVariable(name = "product-name") String productName);
